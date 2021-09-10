636e2a2b9ea920ac672687ba9597f3d8a9e64344
claudiobelempb <claudio.c.lima@hotmail.com>
2021-09-10 08:30:20 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,35 +1,31 @@
-import { BarChart } from "components/BarChart";
-import { DataTable } from "components/DataTable";
-import { DonutChart } from "components/DonutChart";
-import { Footer } from "components/Footer";
-import { NavBar } from "components/NavBar";
+import { BarChart } from 'components/BarChart'
+import { DataTable } from 'components/DataTable'
+import { DonutChart } from 'components/DonutChart'
+import { Footer } from 'components/Footer'
+import { NavBar } from 'components/NavBar'
 
 export function Dashbord() {
   return (
-    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-  <div className="container">
-  <>
-      <NavBar/>
-      <div className="container">
-        <h1 className="text-primary py-3">Dashbord de Vendas</h1>
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-security">Taxa de Sucesso</h5>
-            <BarChart/>
+    <>
+      <NavBar />
+      <div className='container'>
+        <h1 className='text-primary py-3'>Dashbord de Vendas</h1>
+        <div className='row px-3'>
+          <div className='col-sm-6'>
+            <h5 className='text-center text-security'>Taxa de Sucesso</h5>
+            <BarChart />
           </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-security">Todas vendas</h5>
-            <DonutChart/>
+          <div className='col-sm-6'>
+            <h5 className='text-center text-security'>Todas vendas</h5>
+            <DonutChart />
           </div>
         </div>
-        <div className="py-3">
-          <h2 className="text-primary">Todas as Vendas</h2>
+        <div className='py-3'>
+          <h2 className='text-primary'>Todas as Vendas</h2>
         </div>
         <DataTable />
       </div>
       <Footer />
     </>
-  </div>
-</div>
   )
 }
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,22 @@
+import { Footer } from "components/Footer";
+import { NavBar } from "components/NavBar";
+import { Link } from "react-router-dom";
 
 export function Home() {
   return (
-    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-  <div className="container">
-    <h1>Home</h1>
-  </div>
-</div>
+    <>
+      <NavBar />
+      <div className="container">
+      <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+          <hr/>
+          <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+          <Link className={"btn btn-primary btn-lg"} to={"/dashboard"}>Acessar o Dashboard</Link>
+      </div>
+    </div>
+    <Footer/>
+    </>
+
   )
 }
